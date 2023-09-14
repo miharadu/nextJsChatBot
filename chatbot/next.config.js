c9162c0f75cbd4c8862937d97cff6c9ccf5c100a
Mihaela <mihaela-renata.radu@post.at>
2023-09-14 13:46:21 +0200

    react strict mode comment

--- chatbot/next.config.js
+++ chatbot/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export' //https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+    output: 'export', //https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+    // reactStrictMode: true, Since Next.js 13.4, Strict Mode is true by default with app router, so the above configuration is only necessary for pages.
+    // https://nextjs.org/docs/pages/api-reference/next-config-js/reactStrictMode
 }
 
 module.exports = nextConfig
